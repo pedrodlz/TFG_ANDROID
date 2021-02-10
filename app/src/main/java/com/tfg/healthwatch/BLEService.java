f67b701930f301975677a080c3c5dbdf43723dbb
pedrodlz <pedro.zurgena28@hotmail.com>
2021-02-10 20:15:04 +0100

    READ HEAR RATE DONE

---
+++ app/src/main/java/com/tfg/healthwatch/BLEService.java
@@ -0,0 +1,16 @@
+package com.tfg.healthwatch;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BLEService extends Service {
+    public BLEService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
